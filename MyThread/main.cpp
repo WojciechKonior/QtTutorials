d12ef76dd7de057c759b1d0eee19f0530e527471
WK5051 <wkonior@D000556.ilot.local>
2021-05-19 16:37:06 +0200

    MyThread program modification

--- MyThread/main.cpp
+++ MyThread/main.cpp
@@ -5,22 +5,30 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    // Thread definitions
-    MyThread mThr1, mThr2, mThr3, mThr4, mThr5;
-
-    // Setting thread names
+    // 1st thread
+    MyThread mThr1;
     mThr1.name = "Thread 1";
-    mThr2.name = "Thread 2";
-    mThr3.name = "Thread 3";
-    mThr4.name = "Thread 4";
-    mThr5.name = "Thread 5";
-
-    // Running threads with different priorities
     mThr1.start(QThread::HighestPriority);
-    mThr2.start(QThread::NormalPriority);
-    mThr3.start(QThread::LowestPriority);
-    mThr4.start(QThread::HighPriority);
-    mThr5.start(QThread::LowPriority);
+
+    // 2nd thread
+    MyThread mThr2;
+    mThr2.name = "Thread 2";
+    mThr2.start(QThread::HighPriority);
+
+    // 3rd thread
+    MyThread mThr3;
+    mThr3.name = "Thread 3";
+    mThr3.start(QThread::NormalPriority);
+
+    // 4th thread
+    MyThread mThr4;
+    mThr4.name = "Thread 4";
+    mThr4.start(QThread::LowPriority);
+
+    // 5th thread
+    MyThread mThr5;
+    mThr5.name = "Thread 5";
+    mThr5.start(QThread::LowestPriority);
 
     return a.exec();
 }
